7adb9618831ac925362b8c65e20c87302389c208
felipefer <felipe11@astro.ufrj.br>
2017-05-25 16:49:06 -0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='eita',
+      version='0.0',
+      description='Evolutionary Tracks and Isochrones: Tools and Applications',
+      url='',
+      author='Felipe de Almeida Fernandes',
+      author_email='felipe11@astro.ufrj.br',
+      license='',
+      packages=['eita'],
+      zip_safe=False)
