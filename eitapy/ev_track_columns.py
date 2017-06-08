1774227766a4134d7d7117124906c3d92d70eb74
felipefer <felipe11@astro.ufrj.br>
2017-06-08 15:27:57 -0300

    Minor fixes with pycharm

--- eitapy/ev_track_columns.py
+++ eitapy/ev_track_columns.py
@@ -45,47 +45,139 @@ age = Ev_track_column(name        = "stellar_age",
 age.add_PARSEC_info(PARSEC_col_name = "AGE",
                     PARSEC_col_id   = 2)
 
+################################################################################
+
 log_age = Ev_track_column(name        = "log_stellar_age",
                           description = "age of the star",
                           unit        = "years",
                           log         = True)
 
 #\TODO include way to know these quantities are related
+################################################################################
 
 initial_mass = Ev_track_column(name        = "initial_mass",
                                description = "initial stellar mass",
                                unit        = "M_sun",
                                log         = False)
 
+################################################################################
+
 mass = Ev_track_column(name        = "mass",
                        description = "current stellar mass",
                        unit        = "M_sun",
                        log         = False)
+
+mass.add_PARSEC_info(PARSEC_col_name = "MASS",
+                     PARSEC_col_id   = 1)
+
+################################################################################
+
+log_L = Ev_track_column(name        = "log_L",
+                        description = "Log bolometric luminosity",
+                        unit        = "L_sun",
+                        log         = True)
+
+log_L.add_PARSEC_info(PARSEC_col_name = "LOG_L",
+                      PARSEC_col_id   = 3)
+
+################################################################################
+
+log_Teff = Ev_track_column(name        = "log_Teff",
+                           description = "Log effective temperature",
+                           unit        = "K",
+                           log         = True)
+
+log_Teff.add_PARSEC_info(PARSEC_col_name = "LOG_TE",
+                         PARSEC_col_id   = 4)
+
+################################################################################
+
+log_R = Ev_track_column(name        = "log_R",
+                        description = "Log radius",
+                        unit        = "R_sun",
+                        log         = True)
+
+log_R.add_PARSEC_info(PARSEC_col_name = "LOG_R",
+                      PARSEC_col_id   = 5)
+
+################################################################################
 
 mdot = Ev_track_column(name        = "mdot",
                        description = "mass loss rate",
                        unit        = "M_sun/year",
                        log         = False)
 
+mdot.add_PARSEC_info(PARSEC_col_name = "LOG_RAT",
+                     PARSEC_col_id   = 6)
+
+################################################################################
+
 he_core_mass = Ev_track_column(name        = "he_core_mass",
                                description = "mass of helium rich core",
                                unit        = "M_sun",
                                log         = False)
+
+he_core_mass.add_PARSEC_info(PARSEC_col_name = "M_CORE_HE",
+                             PARSEC_col_id   = 7)
+
+################################################################################
 
 c_core_mass = Ev_track_column(name        = "c_core_mass",
                               description = "mass of carbon rich core",
                               unit        = "M_sun",
                               log         = False)
 
+c_core_mass.add_PARSEC_info(PARSEC_col_name = "M_CORE_C",
+                            PARSEC_col_id   = 8)
+
+################################################################################
+
+center_H = Ev_track_column(name        = "center_H",
+                           description = "center mass fraction in Hydrogen",
+                           unit        = "",
+                           log         = False)
+
+center_H.add_PARSEC_info(PARSEC_col_name = "H_CEN",
+                          PARSEC_col_id   = 9)
+
+################################################################################
+
+center_He = Ev_track_column(name        = "center_He",
+                            description = "center mass fraction in Helium",
+                            unit        = "",
+                            log         = False)
+
+center_He.add_PARSEC_info(PARSEC_col_name = "HE_CEN",
+                          PARSEC_col_id   = 10)
+
+################################################################################
+
+center_C = Ev_track_column(name        = "center_C",
+                           description = "center mass fraction in Carbon",
+                           unit        = "",
+                           log         = False)
+
+center_C.add_PARSEC_info(PARSEC_col_name = "C_cen",
+                         PARSEC_col_id   = 11)
+
+################################################################################
+
+center_O = Ev_track_column(name        = "center_O",
+                           description = "center mass fraction in Oxygen",
+                           unit        = "",
+                           log         = False)
+
+center_O.add_PARSEC_info(PARSEC_col_name = "O_cen",
+                         PARSEC_col_id   = 12)
+
+################################################################################
+
 o_core_mass = Ev_track_column(name        = "o_core_mass",
                               description = "mass of oxygen rich core",
                               unit        = "M_sun",
                               log         = False)
 
-log_L = Ev_track_column(name        = "log_L",
-                        description = "Log bolometric luminosity",
-                        unit        = "L_sun",
-                        log         = True)
+################################################################################
 
 log_L_div_Leed = Ev_track_column(name        = "log_L_div_Leed",
                                  description = ("Log ratio of bolometric "
@@ -94,15 +186,61 @@ log_L_div_Leed = Ev_track_column(name        = "log_L_div_Leed",
                                  unit        = "",
                                  log         = True)
 
+################################################################################
+
 log_LH = Ev_track_column(name        = "log_LH",
                          description = "Log hydrogen-burning luminosity",
                          unit        = "L_sun",
                          log         = True)
 
+################################################################################
+
+LH_frac = Ev_track_column(name        = "LH_frac",
+                         description = "hydrogen-burning luminosity fraction",
+                         unit        = "",
+                         log         = True)
+
+LH_frac.add_PARSEC_info(PARSEC_col_name = "LX",
+                        PARSEC_col_id   = 13)
+
+################################################################################
+
 log_LHe = Ev_track_column(name        = "log_LHe",
                           description = "Log helium-burning luminosity",
                           unit        = "L_sun",
                           log         = True)
+
+################################################################################
+
+LHe_frac = Ev_track_column(name       = "LHe_frac",
+                          description = "helium-burning luminosity fraction",
+                          unit        = "",
+                          log         = True)
+
+LHe_frac.add_PARSEC_info(PARSEC_col_name = "LY",
+                         PARSEC_col_id   = 14)
+
+################################################################################
+
+LC_frac = Ev_track_column(name        = "LC_frac",
+                          description = "carbon-burning luminosity fraction",
+                          unit        = "",
+                          log         = True)
+
+LC_frac.add_PARSEC_info(PARSEC_col_name = "LC",
+                        PARSEC_col_id   = 15)
+
+################################################################################
+
+LNeutr_frac = Ev_track_column(name        = "LNeutr_frac",
+                              description = "neutrino luminosity fraction",
+                              unit        = "",
+                              log         = True)
+
+LNeutr_frac.add_PARSEC_info(PARSEC_col_name = "LNEUTR",
+                            PARSEC_col_id   = 16)
+
+################################################################################
 
 log_LZ = Ev_track_column(name        = "log_LZ",
                          description = ("Log total-burning luminosity "
@@ -111,26 +249,32 @@ log_LZ = Ev_track_column(name        = "log_LZ",
                          unit        = "L_sun",
                          log         = True)
 
+################################################################################
+
 log_Lgrav = Ev_track_column(name        = "log_Lgrav",
                             description = ("Log gravitational potential "
                                            "luminosity"),
                             unit        = "L_sun",
                             log         = True)
 
-log_Teff = Ev_track_column(name        = "log_Teff",
-                           description = "Log effective temperature",
-                           unit        = "K",
-                           log         = True)
+################################################################################
 
-log_R = Ev_track_column(name        = "log_R",
-                        description = "Log radius",
-                        unit        = "R_sun",
-                        log         = True)
+Lgrav_frac = Ev_track_column(name        = "Lgrav_frac",
+                             description = "gravity luminosity fraction",
+                             unit        = "",
+                             log         = True)
+
+Lgrav_frac.add_PARSEC_info(PARSEC_col_name = "L_GRAV",
+                           PARSEC_col_id   = 17)
+
+################################################################################
 
 log_g = Ev_track_column(name        = "log_g",
                         description = "Log surface gravity",
                         unit        = "cm s-2",
                         log         = True)
+
+################################################################################
 
 log_surf_Z = Ev_track_column(name        = "log_surf_Z",
                              description = ("Log surface mass fraction in "
@@ -138,15 +282,21 @@ log_surf_Z = Ev_track_column(name        = "log_surf_Z",
                              unit        = "",
                              log         = True)
 
+################################################################################
+
 surf_avg_omega = Ev_track_column(name        = "surf_avg_omega",
                                  description = "surface angular rotation speed",
                                  unit        = "", # ?
                                  log         = False)
 
+################################################################################
+
 surf_avg_v_rot = Ev_track_column(name        = "surf_avg_v_rot",
                                  description = "surface rotation speed",
                                  unit        = "", # ?
                                  log         = False)
+
+################################################################################
 
 c12_div_o16 = Ev_track_column(name        = "surf_num_c12_div_num_o16",
                               description = ("ratio of surface number densities"
@@ -154,9 +304,72 @@ c12_div_o16 = Ev_track_column(name        = "surf_num_c12_div_num_o16",
                               unit        = "",
                               log         = False)
 
-v_wind = Ev_track_column(name             = "v_wind",
-                              description = "wind speed",
-                              unit        = "km s-1",
-                              log         = False)
+################################################################################
+
+v_wind = Ev_track_column(name        = "v_wind",
+                         description = "wind speed",
+                         unit        = "km s-1",
+                         log         = False)
+
+################################################################################
+
+surf_H = Ev_track_column(name        = "surf_H",
+                         description = "surface mass fraction in Hydrogen",
+                         unit        = "",
+                         log         = False)
+
+surf_H.add_PARSEC_info(PARSEC_col_name = "H_SUP",
+                       PARSEC_col_id   = 18)
+
+################################################################################
+
+surf_He = Ev_track_column(name        = "surf_He",
+                          description = "surface mass fraction in Helium",
+                          unit        = "",
+                          log         = False)
+
+surf_He.add_PARSEC_info(PARSEC_col_name = "HE_SUP",
+                        PARSEC_col_id   = 19)
+
+################################################################################
+
+surf_C = Ev_track_column(name        = "surf_C",
+                         description = "surface mass fraction in Carbon",
+                         unit        = "",
+                         log         = False)
+
+surf_C.add_PARSEC_info(PARSEC_col_name = "C_SUP",
+                       PARSEC_col_id   = 20)
+
+################################################################################
+
+surf_N = Ev_track_column(name        = "surf_N",
+                         description = "surface mass fraction in Nitrogen",
+                         unit        = "",
+                         log         = False)
+
+surf_N.add_PARSEC_info(PARSEC_col_name = "N_SUP",
+                       PARSEC_col_id   = 21)
+
+################################################################################
+
+surf_O = Ev_track_column(name        = "surf_O",
+                         description = "surface mass fraction in Oxygen",
+                         unit        = "",
+                         log         = False)
+
+surf_O.add_PARSEC_info(PARSEC_col_name = "O_SUP",
+                       PARSEC_col_id   = 22)
+
 # \WARNING different track sets may use different phases definition.
 # Use for the columns PARSEC_phase, MIST_phase, etc.
+
+################################################################################
+
+phase = Ev_track_column(name        = "phase",
+                        description = "attributed evolutionary phase",
+                        unit        = "",
+                        log         = False)
+
+phase.add_PARSEC_info(PARSEC_col_name = "PHASE",
+                      PARSEC_col_id   = 23)
--- eitapy/load.py
+++ eitapy/load.py
@@ -2,7 +2,9 @@
 load.py contains the functions used to load evolutionary tracks and isochrones
 """
 
+import numpy as np
 import utils
+import ev_track_columns as etcol
 
 ################################################################################
 
@@ -12,20 +14,20 @@ class LoadedEvolutionaryTrack(object):
     Contains the data of an evolutionary track file and is used to initiate an 
     object of the class Ev_Track
     """
-
+    
     def __init__(self, mass, Z, file_format = 'PARSEC'):
         """
         param file_format: format used in the evolutionary track file
         """
-
+        
         self.loaded = False
-
+        
         self.mass = mass
         self.Z    = Z
-
+        
         # Calculate Helium abundances
         self.Y = utils.abundanceY(Z)
-
+        
         self.file_format = file_format
 
     def load(self, path):
@@ -34,24 +36,43 @@ class LoadedEvolutionaryTrack(object):
         
         param path: path leading to the file containing the data
         """
-
+        
         if self.file_format == 'PARSEC':
-            self._load_parsec()
-
-    def _load_parsec(self):
+            self._load_parsec(path)
+    
+    def _load_parsec(self, path):
         """
         used internally to load parsec data format
         """
-
+        
         filename  = parsec_filename(Z    = self.Z,
                                     Y    = self.Y,
                                     mass = self.mass)
-
+        
         directory = parsec_directory(Z   = self.Z,
                                      Y   = self.Y)
-
+        
         self.filename = directory+'/'+filename
-
+        
+        # Load PARSEC file into ev_track_data
+        ev_track_data = np.loadtxt(path+'/'+filename, skiprows = 1)
+        
+        # Columns that are present in the PARSEC tracks
+        columns = [etcol.mass, etcol.age, etcol.log_L, etcol.log_Teff,
+                   etcol.log_R, etcol.mdot, etcol.he_core_mass,
+                   etcol.c_core_mass, etcol.center_H, etcol.center_He,
+                   etcol.center_C, etcol.center_O, etcol.LH_frac,
+                   etcol.LHe_frac, etcol.LC_frac, etcol.LNeutr_frac,
+                   etcol.Lgrav_frac, etcol.surf_H, etcol.surf_He, etcol.surf_C,
+                   etcol.surf_N, etcol.surf_O, etcol.phase]
+        
+        self.column_names = []
+        
+        # Loading columns data as attributes
+        for col in columns:
+            value = ev_track_data[:, col.PARSEC_col_id]
+            self.__setattr__(col.name, value)
+            self.column_names.append(col.name)
 
 ################################################################################
 
@@ -60,11 +81,11 @@ def parsec_filename(Z, Y, mass):
     Get default name for PARSEC evolutionary tracks with mass M, and 
     compositions Y, Z
     """
-
+    
     Z_fmt = str(Z)
     Y_fmt = str(Y)
     OUTA = '1.77' if mass <= 0.7 else '1.74'
-
+    
     return "Z{:s}Y{:s}OUTA{:s}_F7_M{:07.3f}.DAT".format(Z_fmt, Y_fmt,
                                                         OUTA,  mass)
 
@@ -74,8 +95,8 @@ def parsec_directory(Z, Y):
     Get the default PARSEC name for the folder containing data for composition 
     Y, Z.
     """
-
+    
     Z_fmt = str(Z)
     Y_fmt = str(Y)
-
+    
     return "Z{:s}Y{:s}".format(Z_fmt, Y_fmt)
