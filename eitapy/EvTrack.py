e1c28244f29f0d128c2987641f788f96752a4db5
Felipefer <felipe11@astro.ufrj.br>
2018-03-09 19:17:10 -0300

    Include Isochrone class, adapted load and ev_track_columns to load isochrones and included isochrone load test.

--- eitapy/EvTrack.py
+++ eitapy/EvTrack.py
@@ -47,7 +47,9 @@ class EvTrack(object):
         
         # Load EvTrack according to the file format
         EvTrackData = None
-        
+
+        # Todo: Review this. By design, all model dependent characteristics
+        # Todo: should be dealt with in the load.py
         if model == 'PARSEC':
             EvTrackData = load.LoadedEvolutionaryTrack(mass = mass,
                                                        Z = Z,
@@ -60,7 +62,7 @@ class EvTrack(object):
         # End of loading
         # Check if loading went fine
         if EvTrackData is None:
-            raise utils.Loading("Could not load evolutionary track file")
+            raise utils.LoadingError("Could not load evolutionary track file")
         
         # Attributes
         self.mass = mass
@@ -1326,6 +1328,77 @@ class EvTrack_MassSet(object):
 
             # Update self data
             self.array[i, :, :] = track_i.array
+
+
+class Isochrone(object):
+    # \TODO expand this docstring
+    """
+
+    """
+
+    def __init__(self, age = None, Z = None, model='Not_Assigned', path=None,
+                 array=None, columns=None):
+
+        # Check if given model is supported in this version of eitapy
+        if model not in load.allowed_models:
+            raise ValueError(("{0} is not a supported file format.\n"
+                              "Supported formats are {0}."
+                              "").format(load.allowed_models))
+
+        # Load EvTrack according to the file format
+        Isochrone = None
+
+        IsochroneData = load.LoadedIsochrone(age=age,
+                                             Z=Z,
+                                             model=model,
+                                             path=path,
+                                             array=array,
+                                             columns=columns
+                                             )
+
+        # End of loading
+        # Check if loading went fine
+        if IsochroneData is None:
+            raise utils.LoadingError("Could not load isochrone file")
+
+        # Attributes
+        self.age = age
+        self.Z = Z
+        self.Y = IsochroneData.Y
+
+        self.model = model
+        self.column_names = IsochroneData.column_names
+        self.column_fmt = IsochroneData.column_fmt
+
+        # Get number of columns
+        self.Ncols = len(self.column_names)
+
+        # Get number of lines
+        self.Nlines = len(getattr(IsochroneData, self.column_names[0]))
+
+        # \todo WARNING!!!
+        # Here I'm going to use python's ability to reference two variables to
+        # the same object. Data will be stored in self.array, and attributes
+        # like self.age will point to a specific column of this array.
+        #
+        # This way, if you change self.array, self.name_of_the_column will also
+        # change. But if you change self.name_of_the_column, only this attribute
+        # will change, because you will be creating a new instance for that
+        # attribute. Be careful!!!
+
+        # Create data array:
+        self.array = np.empty([self.Nlines, self.Ncols], dtype=float)
+        self.array[:] = np.nan
+        self.column_index = {}
+
+        # Fill the array and point the attributes to it:
+        for i in range(self.Ncols):
+            column = self.column_names[i]
+
+            self.column_index[column] = i
+            self.array[:, i] = getattr(IsochroneData, column)
+            setattr(self, column, self.array[:, i])
+
 
 def save(self):
         # \TODO implement
